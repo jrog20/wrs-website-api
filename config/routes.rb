0bc1ec8afac5167c9b99d474f9e3020372143634
jrog20 <jaimerogstad@gmail.com>
2021-08-13 16:16:22 -0400

    Add routes for sessions methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/get_current_user', to: 'sessions#get_current_user'
+
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
