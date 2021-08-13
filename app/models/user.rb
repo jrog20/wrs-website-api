465fa183e7616b4e726c17b296742e01ce666dfe
jrog20 <jaimerogstad@gmail.com>
2021-08-13 16:32:07 -0400

    Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+  class User < ApplicationRecord
+    has_secure_password
+    validates :email, :username, presence: true
+    validates :email, :username, uniqueness: true
+  
+    # Should this be refactored/moved to a serializer?
+    def user_serializer
+      {
+        username: username,
+        email: email,
+        id: id
+      }
+    end
+  end
 end
