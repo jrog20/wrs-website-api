e3b12114489239c8f08e467ac67ee9284a440a3a
jrog20 <jaimerogstad@gmail.com>
2021-08-13 16:50:16 -0400

    Fix multiple line error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,14 @@
 class User < ApplicationRecord
-  class User < ApplicationRecord
-    has_secure_password
-    validates :email, :username, presence: true
-    validates :email, :username, uniqueness: true
-  
-    # Should this be refactored/moved to a serializer?
-    def user_serializer
-      {
-        username: username,
-        email: email,
-        id: id
-      }
-    end
+  has_secure_password
+  validates :email, :username, presence: true
+  validates :email, :username, uniqueness: true
+
+  # Should this be refactored/moved to a serializer?
+  def user_serializer
+    {
+      username: username,
+      email: email,
+      id: id
+    }
   end
 end
