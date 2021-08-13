619f3bfd18dd8599713d1f8fd8195119b4401714
jrog20 <jaimerogstad@gmail.com>
2021-08-13 16:21:01 -0400

    Add sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,39 @@
+class SessionsController < ApplicationController
+  def create
+    # Changing from email: params[:user][:email] to 
+    # email: params[:email] stops the 500 error, but redirects to home
+    binding.pry
+    @user = User.find_by(email: params[:session][:email])
+    # Changed from params[:user][:password]
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      response = {
+        user: @user.user_serializer
+      }
+      render json: response, status: :ok
+    else
+      response = {
+        error: "Invalid login",
+        details: @user.errors.full_messages
+      }
+      render json: response, status: :unauthorized
+    end
+  end
+
+  def get_current_user
+    if logged_in?
+      render json: {
+          user: current_user.user_serializer
+        }, status: :ok
+    else
+      render json: {error: "No current user"}
+    end
+  end
+
+  def destroy
+    session.clear
+    render json: {
+      message: "Successfully logged out"
+    }
+  end
+end
