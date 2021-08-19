ad47b3fc01c43b78b9a76c8e87432033ddedca19
jrog20 <jaimerogstad@gmail.com>
2021-08-19 18:55:13 -0400

    Change db from sqlite to postgres

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,10 +1,10 @@
 class PostSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
-  attributes :id, :title, :body, :date, :image, :tags, :categories
-  def image
-    if object.image.attached?
+  attributes :id, :title, :body, :date, :images, :tags, :categories
+  def images
+    if object.images.attached?
       {
-        url: rails_blob_url(object.image)
+        url: rails_blob_url(object.images)
       }
     end
   end
