4d700535cc688892a70411e5075c628893f302ff
jrog20 <jaimerogstad@gmail.com>
2021-08-23 17:00:28 -0400

    Get image to be allowed in params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,9 +6,9 @@ class PostsController < ApplicationController
 
   def create
     # Change from strong params to allow image
-    @post = Post.create(title: params[:title], body: params[:body], date: params[:date], tags: params[:tags], categories: params[:categories], images: params[:images])
-    # binding.pry
     # @post = Post.create(post_params)
+    @post = Post.create(title: params[:title], body: params[:body], date: params[:date], tags: params[:tags => []], categories: params[:categories => []], images: params[:images])
+    # binding.pry
     # @post.images.attach(params[:images])
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
   get '/get_current_user', to: 'sessions#get_current_user'
 
-  post "/posts", to: 'posts#create'
-  get "/posts", to: "posts#index"
-  delete "/post/:id", to: "posts#destroy"
+  post '/posts', to: 'posts#create'
+  get '/posts', to: 'posts#index'
+  delete '/post/:id', to: 'posts#destroy'
 
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
