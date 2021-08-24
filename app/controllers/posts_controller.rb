67010471a87eb98296c74f4390ec1fe0f83b5dc2
jrog20 <jaimerogstad@gmail.com>
2021-08-24 15:05:28 -0400

    Fix image url issue

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,14 +2,13 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
     render json: @posts
-    # binding.pry
   end
 
   def create
     # Change from strong params to allow image
     # @post = Post.create(post_params)
-    @post = Post.create(title: params[:title], body: params[:body], date: params[:date], tags: params[:tags], categories: params[:categories], images: params[:images])
     # binding.pry
+    @post = Post.create(title: params[:title], body: params[:body], date: params[:date], tags: params[:tags], categories: params[:categories], images: params[:images])
     # @post.images.attach(params[:images])
   end
 
--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -2,11 +2,10 @@ class PostSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
   attributes :id, :title, :body, :date, :tags, :categories, :images
 
-  # Do I need object.images[0]?
   def images
     if object.images.attached?
       {
-        url: rails_blob_url(object.images, only_path: true)
+        url: "http://localhost:3001#{rails_blob_path(object.images[0], only_path: true)}"
       }
     end
   end
