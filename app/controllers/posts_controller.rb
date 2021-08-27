72c99724434cb6836008cbce9b21462661c5eb7e
jrog20 <jaimerogstad@gmail.com>
2021-08-27 10:37:01 -0400

    Remove comments and change from instance variables to local variables

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,30 +1,29 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
-    render json: @posts
+    posts = Post.all
+    render json: posts
   end
 
-  # How is this going to work for the image?
   def show
-    render json: @post
+    render json: post
   end
 
   def create
-    @post = Post.new(post_params)
-    @post.images.attach(params[:images])
-    @post.save
+    post = Post.new(post_params)
+    post.images.attach(params[:images])
+    post.save
   end
 
   def update
-    if @post.update(post_params)
-      render json: @post
+    if post.update(post_params)
+      render json: post
     else
-      render json: @post.errors, status: :unprocessable_entity
+      render json: post.errors, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @post.destroy
+    post.destroy
   end
 
   private
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,35 +1,21 @@
 class SessionsController < ApplicationController
   def create
-    # Changing from email: params[:user][:email] to 
-    # email: params[:email] stops the 500 error, but redirects to home
-  # binding.pry
-    # *** MAYBE THIS? ***:     
-    # @user = User.find_by_email(params[:email])
-    # if @user && @user.authenticate(params[:password])
-
-    @user = User.find_by(email: params[:session][:email])
-    # Changed from params[:user][:password]
-    if @user && @user.authenticate(params[:session][:password])
-      session[:user_id] = @user.id
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
       response = {
-        user: @user.user_serializer
+        user: user.user_serializer
       }
       render json: response, status: :ok
-      # render json: UserSerializer.new(@user), status: :ok
-      # render json: @user
-      # render json: user: @user.user_serializer
     else
       render json: {
         error: "Invalid login"
-        # details: @user.errors.full_messages
       }
-      # render json: response, status: :unauthorized
     end
   end
 
   def get_current_user
     if logged_in?
-      # render json: UserSerializer.new(current_user)
       render json: {
           user: current_user.user_serializer
         }, status: :ok
